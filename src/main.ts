14ae469842ef3539fd07b44c543c552484ad88a7
Mariano Hurtado de Mendoza Carranza <mariano.hmc@hotmail.com>
2025-11-29 14:44:36 -0600

    added placeholder player sprite

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,26 @@
 import Phaser from "phaser";
+import mage from "../public/placeholders/mage_placeholder.png";
+class MainScene extends Phaser.Scene {
+  constructor(){
+    super({key: "main-scene"});
+  }
+
+  preload(){
+    this.load.spritesheet("mage", mage, {frameWidth: 19, frameHeight: 16})
+  }
+
+  create(){
+    const player = this.add.sprite(this.game.config.width as number/2, this.game.config.height as number/2, "mage");
+    player.scale *= 4;
+  }
+}
 
 const config: Phaser.Types.Core.GameConfig = {
   width: 240 * 2,
   height: 160 * 2,
   parent: "main",
-  scene: {
-    preload,
-    create,
-    update,
-  }
+  pixelArt: true,
+  scene: [MainScene]
 }
 
 new Phaser.Game(config);
-
-function preload(){};
-function create(){};
-function update(){};
